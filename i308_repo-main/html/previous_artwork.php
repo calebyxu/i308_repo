60f0f4da857107dc3573f3838ec185f853ea026f
ralmarar <ralmarar@iu.edu>
2025-04-26 23:06:51 -0400

    one last change

--- i308_repo-main/html/previous_artwork.php
+++ i308_repo-main/html/previous_artwork.php
@@ -6,43 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Previous Artwork</title>
 
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 
 <body>
 
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">Artisan Artwork</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="inventory.html">Inventory</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="previous_artwork.html">Previous Artwork</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="development.html">Artwork in Development</a></li>
-                    <li class="nav-item"><a class="nav-link" href="client.html">Client List</a></li>
-                    <li class="nav-item"><a class="nav-link" href="ideas.html">Project Ideas</a></li>
-                    <li class="nav-item"><a class="nav-link" href="dreamlist.html">Dream List</a></li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">Artisan Artwork</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="inventory.html">Inventory</a></li>
+                <li class="nav-item"><a class="nav-link active" href="previous_artwork.php">Previous Artwork</a></li>
+                <li class="nav-item"><a class="nav-link" href="development.html">Artwork in Development</a></li>
+                <li class="nav-item"><a class="nav-link" href="client.html">Client List</a></li>
+                <li class="nav-item"><a class="nav-link" href="ideas.html">Project Ideas</a></li>
+                <li class="nav-item"><a class="nav-link" href="dreamlist.html">Dream List</a></li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
+<div class="container mt-5">
+    <h1 class="text-center">Previous Artwork</h1>
+    <p class="lead text-center">Browse our collection of completed and sold artwork.</p>
 
-    <div class="container mt-5">
-        <h1 class="text-center">Previous Artwork</h1>
-        <p class="lead text-center">Browse our collection of completed and sold artwork.</p>
-
-
-        
-        <div class="row">
+    <div class="row">
 <?php
 $con = mysqli_connect("db.luddy.indiana.edu", "i308s25_team53", "lodes3344tinge", "i308s25_team53");
 
@@ -83,18 +77,11 @@ if ($result && mysqli_num_rows($result) > 0) {
 mysqli_free_result($result);
 mysqli_close($con);
 ?>
+    </div> 
+
 </div> 
 
-       
-        
-
-
-
-   
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
-
 </html>
